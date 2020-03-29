2ca2a27a157da2a52247fb4a89056d60f11d2ae5
celko.tom@gmail.com <celko.tom@gmail.com>
2020-03-29 16:58:20 +0200

    Add project files.

---
+++ ReaderForClusters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReaderForClusters
+{
+    class Image
+    {
+        int[,] data = new int[256, 256];
+
+    }
+        
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+                
+        }
+    }
+}
